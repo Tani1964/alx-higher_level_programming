b9f04c42fab17dc2a396ea9896c5c0cd6c650aba
Tani1964 <ifegbesan6@gmail.com>
2022-07-18 11:59:05 +0000

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-# TRY
-# WHILE i <= x
-# FOR i in my_list
-# PRINT i
-# increment i by 1
-# EXCEPT 
-# there are no more values to print
     i = 0
     for items in range(x):
         try:
-                    print(my_list[i], end="")
-                    i += 1
+            print(my_list[i], end="")
+            i += 1
         except IndexError:
-            print("No more items in the list")
-
+            break
+    print()
+    return i
